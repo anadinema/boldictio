f10ac9d8a83abce60a9f72bd3bdd36d25c443e30
Anadi Nema <nema.anadi99@gmail.com>
2022-10-01 11:05:47 +0530

    Intial commit

---
+++ src/main/java/me/anadinema/project/boldictio/BoldictioApplication.java
@@ -0,0 +1,13 @@
+package me.anadinema.project.boldictio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoldictioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoldictioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/anadinema/project/boldictio/BoldictioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.anadinema.project.boldictio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoldictioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
